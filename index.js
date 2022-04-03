e8c100edff35ecc3fb77fe44ad0f4dbff8e27215
Milan Gurung <milan.gurungnalim@gmail.com>
2022-04-03 12:12:57 +0545

    Initial Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// Import packages
+const express = require("express");
+const home = require("./routes/home");
+
+// Middlewares
+const app = express();
+app.use(express.json());
+
+// Routes
+app.use("/home", home);
+
+// connection
+const port = process.env.PORT || 9001;
+app.listen(port, () => console.log(`Listening to port ${port}`));
---
+++ routes/home.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  return res.status(200).json({
+    title: "Express Testing",
+    message: "The app is working properly!",
+  });
+});
+
+module.exports = router;

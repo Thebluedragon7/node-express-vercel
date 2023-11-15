266cf9ec07a473596f9487cd2eae3fe7c9ae24fc
yonastatek <yonastatekcse1@gmail.com>
2023-11-15 20:38:32 +0300

    change the value

--- index.js
+++ index.js
@@ -10,7 +10,25 @@ app.use(express.json());
 app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(async(req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next()
+})
 // Routes
+
 app.use("/", home);
 app.use("/chapa", chapa);
 

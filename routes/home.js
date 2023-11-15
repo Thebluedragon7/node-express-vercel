902752db5922a744a96f313c78bfd5b11dbdd79e
yonastatek <yonastatekcse1@gmail.com>
2023-11-15 19:29:57 +0300

    change the value

--- routes/home.js
+++ routes/home.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.use(cors())
 router.get("/", async (req, res, next) => {
   return res.status(200).json({
-    title: "Express Testing",
+    title: "node Express Testing",
     message: "This is app is for chapa testing for ewma",
   });
 });

61f362f2da842df67807d0e48be492f6376b6644
yonastatek <yonastatekcse1@gmail.com>
2023-11-15 19:44:57 +0300

    change the value

--- routes/chapa.js
+++ routes/chapa.js
@@ -21,6 +21,16 @@ const customerInfo =  {
         description: 'It is time to pay'
     }
 }
+res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+// Request headers you wish to allow
+res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+// Set to true if you need the website to include cookies in the requests sent
+// to the API (e.g. in case you use sessions)
+res.setHeader('Access-Control-Allow-Credentials', true);
+
 
 myChapa.initialize(customerInfo, { autoRef: true }).then(response => {
  
